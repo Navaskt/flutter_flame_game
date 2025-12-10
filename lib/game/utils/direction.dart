f394fc5e6f3b5b669d8de02699a10dbcbfcce671
Mohammed Navas <navaskt38@gmail.com>
2025-12-10 15:38:20 +0400

    Fix direction comparison logic in DirectionExtension

--- lib/game/utils/direction.dart
+++ lib/game/utils/direction.dart
@@ -4,26 +4,26 @@ extension DirectionExtension on Direction {
   bool isOpposite(Direction other) {
     switch (this) {
       case Direction.up:
-        return other == Direction.down;
-      case Direction.down:
         return other == Direction.up;
+      case Direction.down:
+        return other == Direction.down;
       case Direction.left:
-        return other == Direction.right;
-      case Direction.right:
         return other == Direction.left;
+      case Direction.right:
+        return other == Direction.right;
     }
   }
-  
+
   Direction get opposite {
     switch (this) {
       case Direction.up:
-        return Direction.down;
-      case Direction.down:
         return Direction.up;
-      case Direction. left:
-        return Direction.right;
-      case Direction.right:
+      case Direction.down:
+        return Direction.down;
+      case Direction.left:
         return Direction.left;
+      case Direction.right:
+        return Direction.right;
     }
   }
 }
